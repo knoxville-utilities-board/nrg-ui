7bad6aead679f741eac1233a36298559e69e308d
jkc07100 <keith.clinard@kub.org>
2024-07-03 12:50:56 -0400

    feat: Add design system package

--- packages/ember-test-app/ember-cli-build.js
+++ packages/ember-test-app/ember-cli-build.js
@@ -6,9 +6,11 @@ module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
     autoImport: {
-      watchDependencies: ['ember-nrg-ui'],
+      watchDependencies: ['ember-nrg-ui', 'design-system'],
     },
   });
+
+  app.import('node_modules/design-system/dist/main.css');
 
   const { maybeEmbroider } = require('@embroider/test-setup');
   return maybeEmbroider(app);
