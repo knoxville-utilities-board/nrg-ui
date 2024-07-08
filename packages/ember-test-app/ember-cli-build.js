d6e614bfc106b2f47a3107a5b1425323dd416171
KeithClinard <keith@clinard.dev>
2024-07-08 12:37:49 -0400

    chore: Fix linking after package rename

--- packages/ember-test-app/ember-cli-build.js
+++ packages/ember-test-app/ember-cli-build.js
@@ -6,11 +6,11 @@ module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
     autoImport: {
-      watchDependencies: ['ember-nrg-ui', 'design-system'],
+      watchDependencies: ['@nrg-ui/ember', '@nrg-ui/css'],
     },
   });
 
-  app.import('node_modules/design-system/dist/main.css');
+  app.import('node_modules/@nrg-ui/css/dist/main.css');
 
   const { maybeEmbroider } = require('@embroider/test-setup');
   return maybeEmbroider(app);
