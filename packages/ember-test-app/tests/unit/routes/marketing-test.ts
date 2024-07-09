3f2d8cde6814ef28cbb5891d199141cf777d00ac
KeithClinard <keith@clinard.dev>
2024-07-09 16:31:16 -0400

    fix: Rename marketing route

--- packages/ember-test-app/tests/unit/routes/marketing-test.ts
+++ packages/ember-test-app/tests/unit/routes/marketing-test.ts
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-test-app/tests/helpers';
 
-module('Unit | Route | marketing-test', function (hooks) {
+module('Unit | Route | marketing', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    const route = this.owner.lookup('route:marketing-test');
+    const route = this.owner.lookup('route:marketing');
     assert.ok(route);
   });
 });
