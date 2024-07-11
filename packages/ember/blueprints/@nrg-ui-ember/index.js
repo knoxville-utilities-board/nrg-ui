4516678bb4a445613a54d4c0c031a0e97b3425f0
tsr07843 <tyler.senter@kub.org>
2024-07-11 14:52:59 -0400

    feat(ember): Add icon fonts to install blueprint

--- packages/ember/blueprints/@nrg-ui-ember/index.js
+++ packages/ember/blueprints/@nrg-ui-ember/index.js
@@ -1,6 +1,21 @@
 /* eslint-env node */
 'use strict';
 
+const BUILD_CHUNK = `  app.import('node_modules/@nrg-ui/css/dist/main.css');
+  app.import(
+    'node_modules/@nrg-ui/css/dist/assets/icons/fonts/bootstrap-icons.woff',
+    {
+      destDir: 'assets/fonts',
+    },
+  );
+  app.import(
+    'node_modules/@nrg-ui/css/dist/assets/icons/fonts/bootstrap-icons.woff2',
+    {
+      destDir: 'assets/fonts',
+    },
+  );
+`;
+
 module.exports = {
   description:
     'The default blueprint for @nrg-ui/ember. This runs when you run `ember install @nrg-ui/ember` in your project.',
@@ -11,20 +26,12 @@ module.exports = {
     //   { name: '@nrg-ui/css' },
     // ]);
 
-    await this.insertIntoFile(
-      'ember-cli-build.js',
-      `  app.import('node_modules/@nrg-ui/css/dist/main.css');\n`,
-      {
-        before:
-          / {2}const { maybeEmbroider } = require\('@embroider\/test-setup'\);/g,
-      },
-    );
-    await this.insertIntoFile(
-      'ember-cli-build.js',
-      `  app.import('node_modules/@nrg-ui/css/dist/main.css');\n\n`,
-      {
-        before: / {2}return app.toTree\(\);/g,
-      },
-    );
+    await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
+      before:
+        / {2}const { maybeEmbroider } = require\('@embroider\/test-setup'\);/g,
+    });
+    await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
+      before: / {2}return app.toTree\(\);/g,
+    });
   },
 };
