8e03b711b7ab74bfb90f79ecd5ff39801b04be72
Tyler Senter <tyler.senter.427@gmail.com>
2024-07-11 08:46:25 -0400

    feat: Add default blueprint and peer dependencies (#3)
    * chore: Add peer dependencies
    
    * feat: Add default blueprint

---
+++ packages/ember/blueprints/@nrg-ui-ember/index.js
@@ -0,0 +1,23 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = {
+  description:
+    'The default blueprint for @nrg-ui/ember. This runs when you run `ember install @nrg-ui/ember` in your project.',
+
+  async afterInstall() {
+    // TODO Add version once it's released
+    // await this.addPackagesToProject([
+    //   { name: '@nrg-ui/css' },
+    // ]);
+
+    await this.insertIntoFile(
+      'ember-cli-build.js',
+      `  app.import('node_modules/@nrg-ui/css/dist/main.css');\n`,
+      {
+        before:
+          / {2}const { maybeEmbroider } = require\('@embroider\/test-setup'\);/g,
+      },
+    );
+  },
+};
