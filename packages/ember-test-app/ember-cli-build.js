d68ea3f89dd6fb1c48fe41d1057ca6cf2f7ef293
Tyler Senter <tyler.senter.427@gmail.com>
2024-07-11 16:06:20 -0400

    feat: Add Bootstrap Icons to design system (#7)
    * feat(design): Install bootstrap-icons
    
    * feat(design): Include icons in build
    
    * feat(ember): Add icon fonts to install blueprint
    
    * feat(test-app): Add build step for icons
    
    * feat(test-app): Add icon to buttons
    
    * chore: PR review

--- packages/ember-test-app/ember-cli-build.js
+++ packages/ember-test-app/ember-cli-build.js
@@ -11,6 +11,18 @@ module.exports = function (defaults) {
   });
 
   app.import('node_modules/@nrg-ui/css/dist/main.css');
+  app.import(
+    'node_modules/@nrg-ui/css/dist/assets/icons/fonts/bootstrap-icons.woff',
+    {
+      destDir: 'assets/fonts',
+    },
+  );
+  app.import(
+    'node_modules/@nrg-ui/css/dist/assets/icons/fonts/bootstrap-icons.woff2',
+    {
+      destDir: 'assets/fonts',
+    },
+  );
 
   const { maybeEmbroider } = require('@embroider/test-setup');
   return maybeEmbroider(app);
