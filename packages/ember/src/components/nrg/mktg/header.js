d310e7d752332d72b44219029a28d315b6bc2733
CJU08539 <chesney.julian@kub.org>
2024-07-17 07:55:19 -0400

    feat: Styling and logic for opinionated header with parameters

--- packages/ember/src/components/nrg/mktg/header.js
+++ packages/ember/src/components/nrg/mktg/header.js
@@ -1,11 +1,12 @@
 import Component from '@glimmer/component';
 
-export default class MarketingHeaderComponent extends Component {
-  icon = this.args.icon;
-  title = this.args.title;
-  options = this.args.options;
-  previousButton = this.args.previousButton;
-  nextButton = this.args.nextButton;
+export default class HeaderComponent extends Component {
+  icon = this.args.icon || null;
+  title = this.args.title || '';
+  options = this.args.options || null;
+  navButtons = this.args.navButtons || false;
+  previousButtonDisabled = this.args.previousButtonDisabled || '';
+  nextButtonDisabled = this.args.nextButtonDisabled || '';
 
   constructor() {
     super(...arguments);
