a3fbc8189f40f380829352527518ea69e4c7449b
CJU08539 <chesney.julian@kub.org>
2024-07-18 12:55:50 -0400

    feat(header): Implement drop down section named block

--- packages/ember/src/components/nrg/mktg/header.js
+++ packages/ember/src/components/nrg/mktg/header.js
@@ -1,12 +1,9 @@
 import Component from '@glimmer/component';
 
 export default class Header extends Component {
-  iconAlignment = this.args.iconAlignment || '';
-  iconAttributes = this.args.iconAttributes || '';
-  titleAlignment = this.args.titleAlignment || '';
-  titleAttributes = this.args.titleAttributes || '';
-  navAlignment = this.args.navAlignment || '';
-  navAttributes = this.args.navAttributes || '';
+  iconClasses = this.args.iconClasses || '';
+  titleClasses = this.args.titleClasses || '';
+  navClasses = this.args.navClasses || '';
 
   constructor() {
     super(...arguments);
