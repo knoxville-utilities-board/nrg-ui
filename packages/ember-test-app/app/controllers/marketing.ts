069dbca0e2ec9ff141d1ee0af98d1bac2f0214cd
CJU08539 <chesney.julian@kub.org>
2024-07-19 15:47:09 -0400

    refactor: Implement getters for named block classes

---
+++ packages/ember-test-app/app/controllers/marketing.ts
@@ -0,0 +1,5 @@
+import Controller from '@ember/controller';
+
+export default class MarketingController extends Controller {
+  classes = ['order-2', 'justify-content-end'];
+}
--- packages/ember/src/components/nrg/mktg/header.js
+++ packages/ember/src/components/nrg/mktg/header.js
@@ -1,11 +1,36 @@
 import Component from '@glimmer/component';
 
 export default class Header extends Component {
-  iconClasses = this.args.iconClasses || '';
-  titleClasses = this.args.titleClasses || '';
-  navClasses = this.args.navClasses || '';
+  get titleClassList() {
+    let classes = ['col', 'd-flex', 'justify-content-center', 'flex-row'];
 
-  constructor() {
-    super(...arguments);
+    if (this.args.titleClasses) {
+      typeof this.args.titleClasses === 'string'
+        ? classes.push(...this.args.titleClasses.split(' '))
+        : classes.push(...this.args.titleClasses);
+    }
+    return classes.join(' ');
+  }
+
+  get brandClassList() {
+    let classes = ['col-2', 'd-flex'];
+
+    if (this.args.brandClasses) {
+      typeof this.args.brandClasses === 'string'
+        ? classes.push(...this.args.brandClasses.split(' '))
+        : classes.push(...this.args.brandClasses);
+    }
+    return classes.join(' ');
+  }
+
+  get navClassList() {
+    let classes = ['col-2', 'd-flex'];
+
+    if (this.args.navClasses) {
+      typeof this.args.navClasses === 'string'
+        ? classes.push(...this.args.navClasses.split(' '))
+        : classes.push(...this.args.navClasses);
+    }
+    return classes.join(' ');
   }
 }
