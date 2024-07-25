d62ed099a7b677d4c30b466890f79ea88166f07b
tpl08527 <trishu.patel@kub.org>
2024-07-25 07:36:18 -0400

    fix: Change namespace in footer test

--- packages/ember-test-app/tests/integration/components/mktg/footer-test.ts
+++ packages/ember-test-app/tests/integration/components/mktg/footer-test.ts
@@ -3,27 +3,27 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | components | nrg/mktg/footer', function (hooks) {
+module('Integration | components | mktg/footer', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    await render(hbs`<Nrg::Mktg::Footer />`);
+    await render(hbs`<Mktg::Footer />`);
 
     assert.dom('footer').exists();
   });
 
   test('it contains a horizontal line', async function (assert) {
-    await render(hbs`<Nrg::Mktg::Footer @hasHorizontalLine={{true}}/>`);
+    await render(hbs`<Mktg::Footer @hasHorizontalLine={{true}}/>`);
 
     assert.dom('footer div hr').exists();
   });
 
   test('it contains a navigation bar', async function (assert) {
-    await render(hbs`<Nrg::Mktg::Footer>
+    await render(hbs`<Mktg::Footer>
       <:nav>
         Nav Test
       </:nav>
-    </Nrg::Mktg::Footer>`);
+    </Mktg::Footer>`);
 
     assert
       .dom('footer div div:first-child div:first-child div')
@@ -32,11 +32,11 @@ module('Integration | components | nrg/mktg/footer', function (hooks) {
   });
 
   test('it contains social media links', async function (assert) {
-    await render(hbs`<Nrg::Mktg::Footer>
+    await render(hbs`<Mktg::Footer>
       <:socialMedia>
         Social Media Test
       </:socialMedia>
-    </Nrg::Mktg::Footer>`);
+    </Mktg::Footer>`);
 
     assert
       .dom('footer div div:first-child div:nth-child(2) div')
@@ -45,11 +45,11 @@ module('Integration | components | nrg/mktg/footer', function (hooks) {
   });
 
   test('it contains a brand section', async function (assert) {
-    await render(hbs`<Nrg::Mktg::Footer>
+    await render(hbs`<Mktg::Footer>
       <:brand>
         Brand Test
       </:brand>
-    </Nrg::Mktg::Footer>`);
+    </Mktg::Footer>`);
 
     assert
       .dom('footer div div:nth-child(2) div:first-child div')
@@ -58,11 +58,11 @@ module('Integration | components | nrg/mktg/footer', function (hooks) {
   });
 
   test('it contains a legal section', async function (assert) {
-    await render(hbs`<Nrg::Mktg::Footer>
+    await render(hbs`<Mktg::Footer>
       <:legal>
         Legal Test
       </:legal>
-    </Nrg::Mktg::Footer>`);
+    </Mktg::Footer>`);
 
     assert
       .dom('footer div div:nth-child(2) div:nth-child(2) div')
