c4001461e5ca6af336d8f3c08bc944b3e09b1ca0
tpl08527 <trishu.patel@kub.org>
2024-07-25 14:05:41 -0400

    chore: Correct test name

--- packages/ember-test-app/tests/integration/components/footer-test.ts
+++ packages/ember-test-app/tests/integration/components/footer-test.ts
@@ -3,7 +3,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | components | button', function (hooks) {
+module('Integration | components | footer', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
