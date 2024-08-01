852560f7414967082a332a2a5f8c0129fb5aa4d4
TSenter <tyler.senter.427@gmail.com>
2024-08-01 14:05:26 -0400

    style: Update syntax color scheme for Handlebars

---
+++ packages/ember-test-app/app/routes/application.ts
@@ -0,0 +1,21 @@
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+
+import type EmberFreestyleService from 'ember-freestyle/services/ember-freestyle';
+
+const syntaxHighlightingTheme = 'github';
+
+export default class ApplicationRoute extends Route {
+  @service('ember-freestyle')
+  declare freestyle: EmberFreestyleService;
+
+  async beforeModel() {
+    this.freestyle.hljsThemeUrl = (theme: string) => {
+      return `https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/${theme}.min.css`;
+    };
+    this.freestyle.hljsVersion = '9.18.5';
+    await this.freestyle.ensureHljs();
+    this.freestyle.ensureHljsTheme(syntaxHighlightingTheme);
+    this.freestyle.defaultTheme = syntaxHighlightingTheme;
+  }
+}
