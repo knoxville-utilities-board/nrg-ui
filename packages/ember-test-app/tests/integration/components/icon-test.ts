41dd8c92be592c91c5f7bdf382840bc9420ae7d5
CJU08539 <chesney.julian@kub.org>
2024-08-01 15:18:31 -0400

    refactor: Update IconBubble to Icon with circular param

---
+++ packages/ember-test-app/tests/integration/components/icon-test.ts
@@ -0,0 +1,49 @@
+import { assert, module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | components | mktg/icon', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('icon renders without circular background', async function () {
+    await render(hbs`
+      <Icon
+        class="test"
+        @type="icon"
+        @color="primary"
+      />`);
+
+    assert.dom('.d-flex.test').exists('Icon renders passed attributes');
+    assert.notOk(
+      document.querySelector('.rounded-circle'),
+      'Icon does not have rounded-circle class when @circular is not passed',
+    );
+    assert
+      .dom('div div i')
+      .hasClass('icon', 'Type param renders icon correctly');
+    assert
+      .dom('div div i')
+      .hasClass('text-primary', 'Color param renders text color correctly');
+  });
+
+  test('icon renders with circular background', async function () {
+    await render(hbs`
+      <Icon
+        class="test"
+        @type="icon"
+        @color="warning"
+        @circular={{true}}
+        @backgroundColor="warning-subtle"
+      />`);
+    assert
+      .dom('.test div')
+      .hasClass(
+        'rounded-circle',
+        'Icon has rounded-circle class when @circular is true',
+      );
+    assert
+      .dom('.test div')
+      .hasClass('bg-warning-subtle', 'Icon bubble has correct bg color passed');
+  });
+});
---
+++ packages/ember/src/components/icon.gts
@@ -0,0 +1,50 @@
+import Component from '@glimmer/component';
+import type { NrgIconValue } from '../';
+
+declare type SubtleColorType =
+  | 'primary-subtle'
+  | 'secondary-subtle'
+  | 'success-subtle'
+  | 'danger-subtle'
+  | 'warning-subtle'
+  | 'info-subtle'
+  | 'light-subtle'
+  | 'dark-subtle';
+
+declare type ColorType =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'danger'
+  | 'warning'
+  | 'info'
+  | 'light'
+  | 'dark';
+
+interface IconSignature {
+  Element: HTMLDivElement;
+  Args: {
+    type: NrgIconValue;
+    backgroundColor?: SubtleColorType | ColorType;
+    color: ColorType;
+    circular?: boolean;
+  };
+}
+
+export default class Icon extends Component<IconSignature> {
+  get classList() {
+    let classes = ['d-flex', 'justify-content-center', 'fs-2', 'm-2'];
+    if (this.args.circular) {
+      classes.push('rounded-circle', 'p-3', `bg-${this.args.backgroundColor}`);
+    }
+    return classes.join(' ');
+  }
+
+  <template>
+    <div class="d-flex" ...attributes>
+      <div class="{{this.classList}}">
+        <i class="bi {{@type}} text-{{@color}}" />
+      </div>
+    </div>
+  </template>
+}
