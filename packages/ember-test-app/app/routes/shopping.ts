4c9cb77cd34ab531eca61bacbf3ce6b7f50d18fb
CJU08539 <chesney.julian@kub.org>
2024-08-07 08:50:01 -0400

    feat: Add media queries for scrolling container

---
+++ packages/ember-test-app/app/routes/shopping.ts
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import type RouterService from '@ember/routing/router-service';
+
+export default class ShoppingRoute extends Route {
+  @service
+  declare router: RouterService;
+
+  redirect() {
+    this.router.transitionTo('shopping.fiber');
+  }
+}
