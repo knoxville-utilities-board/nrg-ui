ef26ff831bf9f55af498f90a91febf4b58d35612
TSenter <tyler.senter.427@gmail.com>
2024-08-09 11:16:38 -0400

    feat: Implement length validator

---
+++ packages/ember-test-app/tests/unit/validators/length-test.ts
@@ -0,0 +1,291 @@
+import { setOwner } from '@ember/application';
+import { tracked } from '@glimmer/tracking';
+import { bind } from '@nrg-ui/ember/helpers/bind';
+import { LengthValidator } from '@nrg-ui/ember/validation/index';
+import { setupIntl } from 'ember-intl/test-support';
+import { setupTest } from 'ember-test-app/tests/helpers';
+import { module, test } from 'qunit';
+
+import type { TestContext as TC } from '@ember/test-helpers';
+import type { types } from '@nrg-ui/ember';
+
+class Model {
+  @tracked
+  field?: ArrayLike<unknown>;
+}
+
+declare type TestContext = {
+  binding: types.Binding;
+  model: Model;
+} & TC;
+
+module('Unit | Validator | length', function (hooks) {
+  setupTest(hooks);
+  setupIntl(hooks, 'en-us');
+
+  hooks.beforeEach(function (this: TestContext) {
+    this.model = new Model();
+    this.binding = bind(this.model, 'field');
+
+    setOwner(this.model, this.owner);
+  });
+
+  test('some option is required', function (this: TestContext, assert) {
+    assert.expect(1);
+
+    assert.throws(() => {
+      const validator = new LengthValidator(this.binding, {}, this.model);
+      const result = validator.result;
+
+      assert.notOk(
+        true,
+        'Expected an error, but got a result instead: ' + result,
+      );
+    }, new Error('Assertion Failed: LengthValidator requires either `is`, `min`, `max`, or `between` to be provided'));
+  });
+
+  test('`allowBlank` option works', function (this: TestContext, assert) {
+    let validator = new LengthValidator(
+      this.binding,
+      { allowBlank: true, is: 5 },
+      this.model,
+    );
+
+    this.model.field = [];
+    let result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+
+    validator = new LengthValidator(
+      this.binding,
+      { allowNone: false, is: 5 },
+      this.model,
+    );
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field must have 5 values (has 0)',
+    });
+  });
+
+  test('`allowNone` option works', function (this: TestContext, assert) {
+    let validator = new LengthValidator(
+      this.binding,
+      { allowNone: false, is: 5 },
+      this.model,
+    );
+
+    let result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field is invalid',
+    });
+
+    validator = new LengthValidator(
+      this.binding,
+      { allowNone: true, is: 5 },
+      this.model,
+    );
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+  });
+
+  test('`is` option works', function (this: TestContext, assert) {
+    const validator = new LengthValidator(this.binding, { is: 5 }, this.model);
+
+    this.model.field = Array.from(Array(10).keys());
+
+    let result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field must have 5 values (has 10)',
+    });
+
+    this.model.field = Array.from(Array(5).keys());
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+  });
+
+  test('`min` option works', function (this: TestContext, assert) {
+    const validator = new LengthValidator(
+      this.binding,
+      { min: 10 },
+      this.model,
+    );
+
+    this.model.field = Array.from(Array(5).keys());
+
+    let result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field must have at least 10 values (has 5)',
+    });
+
+    this.model.field = 'A'.repeat(5);
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field must be at least 10 characters (has 5)',
+    });
+
+    this.model.field = Array.from(Array(10).keys());
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+
+    this.model.field = 'A'.repeat(10);
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+  });
+
+  test('`max` option works', function (this: TestContext, assert) {
+    const validator = new LengthValidator(this.binding, { max: 5 }, this.model);
+
+    this.model.field = Array.from(Array(10).keys());
+
+    let result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field must have at most 5 values (has 10)',
+    });
+
+    this.model.field = 'A'.repeat(10);
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field must be at most 5 characters (has 10)',
+    });
+
+    this.model.field = Array.from(Array(5).keys());
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+
+    this.model.field = 'A'.repeat(5);
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+  });
+
+  test('`between` option works', function (this: TestContext, assert) {
+    const validator = new LengthValidator(
+      this.binding,
+      { between: [5, 10] },
+      this.model,
+    );
+
+    this.model.field = Array.from(Array(4).keys());
+
+    let result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field must have between 5 and 10 values (has 4)',
+    });
+
+    this.model.field = 'A'.repeat(4);
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field must be between 5 and 10 characters (has 4)',
+    });
+
+    this.model.field = Array.from(Array(11).keys());
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field must have between 5 and 10 values (has 11)',
+    });
+
+    this.model.field = 'A'.repeat(11);
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field must be between 5 and 10 characters (has 11)',
+    });
+
+    this.model.field = Array.from(Array(5).keys());
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+
+    this.model.field = 'A'.repeat(5);
+
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+  });
+});
---
+++ packages/ember/src/validation/validators/length.ts
@@ -0,0 +1,120 @@
+import { assert } from '@ember/debug';
+import { isEmpty, isNone } from '@ember/utils';
+
+import BaseValidator from './base.ts';
+
+import type { Binding } from '../../types';
+import type { ValidateFnResponse } from '../types';
+
+export type LengthOptions = {
+  /**
+   * If `true`, the value can be an empty string or array.
+   */
+  allowBlank?: boolean;
+  /**
+   * If `true`, the value can be `null` or `undefined`.
+   */
+  allowNone?: boolean;
+  /**
+   * A range
+   */
+  between?: [number, number];
+  is?: number;
+  max?: number;
+  min?: number;
+};
+
+export default class LengthValidator<
+  T extends ArrayLike<T>,
+  Context extends object = Record<string, unknown>,
+> extends BaseValidator<T, LengthOptions, Context> {
+  defaultOptions = {
+    allowNone: true,
+    presence: true,
+  };
+
+  constructor(binding: Binding, options: LengthOptions, context: Context) {
+    super(binding, options, context);
+
+    if (
+      isNone(options.is) &&
+      isNone(options.min) &&
+      isNone(options.max) &&
+      isNone(options.between)
+    ) {
+      assert(
+        'LengthValidator requires either `is`, `min`, `max`, or `between` to be provided',
+      );
+    }
+  }
+
+  validate(value: T, options: LengthOptions): ValidateFnResponse {
+    const { allowBlank, allowNone, between, is, max, min } = options;
+
+    if (isNone(value)) {
+      if (allowNone) {
+        return true;
+      }
+
+      return { key: 'nrg.validation.length.invalid', value };
+    }
+
+    if (allowBlank && isEmpty(value)) {
+      return true;
+    }
+
+    const type = typeof value === 'string' ? 'string' : 'array';
+    const length = value.length;
+
+    if (!isNone(is) && length !== is) {
+      return {
+        key: `nrg.validation.length.${type}.wrongLength`,
+        is,
+        length,
+        value: this.toString(value),
+      };
+    }
+
+    if (!isNone(min) && length < min) {
+      return {
+        key: `nrg.validation.length.${type}.tooShort`,
+        min,
+        length,
+        value: this.toString(value),
+      };
+    }
+
+    if (!isNone(max) && length > max) {
+      return {
+        key: `nrg.validation.length.${type}.tooLong`,
+        max,
+        length,
+        value: this.toString(value),
+      };
+    }
+
+    if (!isNone(between) && (length < between[0] || length > between[1])) {
+      return {
+        key: `nrg.validation.length.${type}.between`,
+        min: between[0],
+        max: between[1],
+        length,
+        value: this.toString(value),
+      };
+    }
+
+    return true;
+  }
+
+  toString(value: ArrayLike<T>): string {
+    if (typeof value === 'string') {
+      return value;
+    }
+    if (Array.isArray(value)) {
+      return this.intl.formatList(value.map(String));
+    }
+    const values = Array.from(value).map(String);
+
+    return this.intl.formatList(values);
+  }
+}
