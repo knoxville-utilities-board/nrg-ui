d032b8b0873c5a666ccdb4110da024175749cf70
TSenter <tyler.senter.427@gmail.com>
2024-08-09 13:55:41 -0400

    feat: Implement regex validator

---
+++ packages/ember-test-app/tests/unit/validators/regex-test.ts
@@ -0,0 +1,114 @@
+import { setOwner } from '@ember/application';
+import { tracked } from '@glimmer/tracking';
+import { bind } from '@nrg-ui/ember/helpers/bind';
+import { RegexValidator } from '@nrg-ui/ember/validation/index';
+import { setupIntl } from 'ember-intl/test-support';
+import { setupTest } from 'ember-test-app/tests/helpers';
+import { module, test } from 'qunit';
+
+import type { TestContext as TC } from '@ember/test-helpers';
+import type { types } from '@nrg-ui/ember';
+
+class Model {
+  @tracked
+  field?: string;
+}
+
+declare type TestContext = {
+  binding: types.Binding;
+  model: Model;
+} & TC;
+
+module('Unit | Validator | regex', function (hooks) {
+  setupTest(hooks);
+  setupIntl(hooks, 'en-us');
+
+  hooks.beforeEach(function (this: TestContext) {
+    this.model = new Model();
+    this.binding = bind(this.model, 'field');
+
+    setOwner(this.model, this.owner);
+  });
+
+  test('`pattern` is required', function (this: TestContext, assert) {
+    assert.expect(2);
+
+    assert.throws(() => {
+      // @ts-expect-error Testing that the `pattern` option is required
+      const validator = new RegexValidator(this.binding, {}, this.model);
+      const result = validator.result;
+
+      assert.notOk(
+        true,
+        'Expected an error, but got a result instead: ' + result,
+      );
+    }, new Error('Assertion Failed: RegexValidator requires `pattern` to be provided'));
+
+    assert.throws(() => {
+      const validator = new RegexValidator(
+        this.binding,
+        // @ts-expect-error Testing that the `pattern` option is required
+        { pattern: 42 },
+        this.model,
+      );
+      const result = validator.result;
+
+      assert.notOk(
+        true,
+        'Expected an error, but got a result instead: ' + result,
+      );
+    }, new Error('Assertion Failed: RegexValidator requires the pattern to be of type string or RegExp'));
+  });
+
+  test('`pattern` option works', function (this: TestContext, assert) {
+    const validator = new RegexValidator(
+      this.binding,
+      { pattern: /^Foo/ },
+      this.model,
+    );
+
+    this.model.field = 'foo';
+    let result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field is invalid',
+    });
+
+    this.model.field = 'Foo';
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+  });
+
+  test('`inverse` option works', function (this: TestContext, assert) {
+    const validator = new RegexValidator(
+      this.binding,
+      { pattern: /^Foo/, inverse: true },
+      this.model,
+    );
+
+    this.model.field = 'foo';
+    let result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: true,
+      isWarning: false,
+      message: undefined,
+    });
+
+    this.model.field = 'Foo';
+    result = validator.result;
+
+    assert.deepEqual(result, {
+      isValid: false,
+      isWarning: false,
+      message: 'This field is invalid',
+    });
+  });
+});
