f21d6cde14dbbdf23650f33c9bbc82eacd8c8a3a
TSenter <tyler.senter.427@gmail.com>
2024-08-09 11:16:38 -0400

    feat: Implement length validator

--- packages/ember/src/validation/index.ts
+++ packages/ember/src/validation/index.ts
@@ -3,6 +3,7 @@ export { default as ConfirmationValidator } from './validators/confirmation.ts';
 export { default as CustomValidator } from './validators/custom.ts';
 export { default as ExclusionValidator } from './validators/exclusion.ts';
 export { default as InclusionValidator } from './validators/inclusion.ts';
+export { default as LengthValidator } from './validators/length.ts';
 
 export type {
   ComputedProperty,
