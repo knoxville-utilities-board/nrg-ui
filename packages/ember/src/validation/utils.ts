9be89797c05bcfd0613ad54668e9ab6bfa1fab6f
TSenter <tyler.senter.427@gmail.com>
2024-08-12 16:43:29 -0400

    refactor(validators): Resolve type checking errors

--- packages/ember/src/validation/utils.ts
+++ packages/ember/src/validation/utils.ts
@@ -8,6 +8,7 @@ import PresenceValidator from './validators/presence.ts';
 import RangeValidator from './validators/range.ts';
 import RegexValidator from './validators/regex.ts';
 
+import type { TranslatableOption, ValidatorBuilder } from './types';
 import type { Binding } from '../types';
 
 const Validators = {
@@ -21,7 +22,7 @@ const Validators = {
   range: RangeValidator,
   regex: RegexValidator,
 };
-declare type ValidatorType = Lowercase<keyof typeof Validators>;
+type ValidatorType = keyof typeof Validators;
 
 declare type OptionsOf<T extends ValidatorType> = ConstructorParameters<
   (typeof Validators)[T]
@@ -30,56 +31,51 @@ declare type ContextOf<T extends ValidatorType> = ConstructorParameters<
   (typeof Validators)[T]
 >[2];
 
-// TypeScript isn't recognizing that the type of the options has been narrowed
-// to the correct type, so we need to use @ts-expect-error to suppress the error
-// that would otherwise be raised.
-export function validator<V extends ValidatorType>(
-  type: V,
-  options?: OptionsOf<V>,
-) {
+type ValidatorFnArgs<T extends ValidatorType = ValidatorType> = T extends T
+  ? [type: T, values: OptionsOf<T>]
+  : never;
+
+export function validator<V extends ValidatorType = ValidatorType>(
+  ...[type, options]: ValidatorFnArgs<V>
+): ValidatorBuilder<unknown, OptionsOf<V>, ContextOf<V>> {
   if (type === 'confirmation') {
     return (binding: Binding, context: ContextOf<V>) =>
-      // @ts-expect-error - See above
-      new ConfirmationValidator(binding, options, context);
+      new ConfirmationValidator(
+        binding,
+        options,
+        context as Record<string, TranslatableOption>,
+      );
   }
   if (type === 'custom') {
     return (binding: Binding, context: ContextOf<V>) =>
-      // @ts-expect-error - See above
       new CustomValidator(binding, options, context);
   }
   if (type === 'exclusion') {
     return (binding: Binding, context: ContextOf<V>) =>
-      // @ts-expect-error - See above
       new ExclusionValidator(binding, options, context);
   }
   if (type === 'inclusion') {
     return (binding: Binding, context: ContextOf<V>) =>
-      // @ts-expect-error - See above
       new InclusionValidator(binding, options, context);
   }
   if (type === 'length') {
     return (binding: Binding, context: ContextOf<V>) =>
-      // @ts-expect-error - See above
       new LengthValidator(binding, options, context);
   }
   if (type === 'number') {
     return (binding: Binding, context: ContextOf<V>) =>
-      // @ts-expect-error - See above
       new NumberValidator(binding, options, context);
   }
   if (type === 'presence') {
     return (binding: Binding, context: ContextOf<V>) =>
-      // @ts-expect-error - See above
       new PresenceValidator(binding, options, context);
   }
   if (type === 'range') {
     return (binding: Binding, context: ContextOf<V>) =>
-      // @ts-expect-error - Ditto
       new RangeValidator(binding, options, context);
   }
   if (type === 'regex') {
     return (binding: Binding, context: ContextOf<V>) =>
-      // @ts-expect-error - See above
       new RegexValidator(binding, options, context);
   }
 
