ba7a0958d94fce0496efd78b75583d74f371147a
KeithClinard <keith@clinard.dev>
2024-08-23 12:05:21 -0400

    chore: Improve generation of release commit message

--- .github/release-commit-message.js
+++ .github/release-commit-message.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
+const path = require('path');
 
 const releasePlan = fs.readFileSync('.release-plan.json', 'utf8');
 const releasePlanJson = JSON.parse(releasePlan);
 
-const newEmberVersion = releasePlanJson.solution['@nrg-ui/ember'].newVersion;
-const newCssVersion = releasePlanJson.solution['@nrg-ui/css'].newVersion;
+const packageDirectory = 'packages';
+const packages = fs.readdirSync(packageDirectory)
+  .map(file => path.join(packageDirectory, file))
+  .filter(path => fs.statSync(path).isDirectory());
 
 const updatedVersions = [];
-if (newEmberVersion) {
-  updatedVersions.push(`@nrg-ui/ember@${newEmberVersion}`);
-}
-if (newCssVersion) {
-  updatedVersions.push(`@nrg-ui/css@${newCssVersion}`);
+for(const package of packages) {
+  const packageJson = fs.readFileSync(path.join(package, 'package.json'), 'utf8');
+  const packageJsonParsed = JSON.parse(packageJson);
+  if (packageJsonParsed.private) {
+    continue;
+  }
+  const name = packageJsonParsed.name;
+  const newVersion = releasePlanJson.solution[name].newVersion;
+  if (newVersion) {
+    updatedVersions.push(`${name}@${newVersion}`);
+  }
 }
 
 const commitMessage = `COMMIT_MESSAGE=release: ${updatedVersions.join(', ')}`;
