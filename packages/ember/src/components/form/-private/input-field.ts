fe47ac9bacf6060704ac309f8acac745b13849a9
TSenter <tyler.senter.427@gmail.com>
2024-08-28 17:24:56 -0400

    refactor: Move basic input field functionality to a private class

---
+++ packages/ember/src/components/form/-private/input-field.ts
@@ -0,0 +1,45 @@
+import { action } from '@ember/object';
+
+import BoundValue from '../bound-value.ts';
+
+import type { AttrValue } from '@glint/template';
+
+export type InputFieldSignature<S> = {
+  Element: HTMLInputElement;
+  Args: {
+    basic?: boolean;
+    describedBy?: string;
+    disabled?: boolean;
+    id?: string;
+    isInvalid?: boolean;
+    isWarning?: boolean;
+    readonly?: boolean;
+  };
+} & S;
+
+export default class InputField<
+  S = object,
+  T extends AttrValue = string,
+> extends BoundValue<InputFieldSignature<S>, T> {
+  get classList() {
+    const classes = ['form-control'];
+
+    if (this.args.basic) {
+      classes[0] += '-plaintext';
+    }
+
+    if (this.args.isInvalid) {
+      classes.push('is-invalid');
+    } else if (this.args.isWarning) {
+      classes.push('is-warning');
+    }
+
+    return classes.join(' ');
+  }
+
+  @action
+  change(evt: Event) {
+    const target = evt.target as HTMLInputElement;
+    this.onChange(target?.value as T);
+  }
+}
