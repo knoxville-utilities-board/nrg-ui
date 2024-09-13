840ca2f7dcbc04c3f4a3c4268803affb1e8780dc
TSenter <tyler.senter.427@gmail.com>
2024-09-13 13:58:57 -0400

    chore: Trim down test-app

--- apps/test-app/app/controllers/cards.ts
+++
@@ -1,7 +0,0 @@
-import Controller from '@ember/controller';
-
-export default class CardsController extends Controller {
-  clickHandler() {
-    console.log('clickHandler called');
-  }
-}
--- apps/test-app/app/router.ts
+++ apps/test-app/app/router.ts
@@ -6,41 +6,4 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {
-  this.route('cards');
-  this.route('faq');
-  this.route('marketing');
-  this.route('promo');
-  this.route('workflow-tray');
-  this.route('components', function () {
-    this.route('button');
-    this.route('card');
-    this.route('header');
-    this.route('form', function () {
-      this.route('checkbox');
-      this.route('checkbox-group');
-      this.route('phone-input');
-      this.route('radio-group');
-      this.route('select');
-      this.route('text-area');
-      this.route('text-input');
-    });
-    this.route('icon');
-    this.route('navbar');
-    this.route('footer');
-  });
-  this.route('helpers');
-  this.route('mktg-components', function () {
-    this.route('card');
-    this.route('card-container');
-    this.route('faq');
-    this.route('footer');
-    this.route('header');
-    this.route('promo');
-    this.route('promo-container');
-    this.route('section-header');
-    this.route('service-pricing');
-  });
-  this.route('modifiers');
-  this.route('services');
-});
+Router.map(function () {});
