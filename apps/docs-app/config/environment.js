600ec0987af6a04ef24cd695f83bf98fa774fd3b
Tyler Senter <tyler.senter.427@gmail.com>
2024-09-16 16:36:59 -0400

    chore: Update rootURL for docs app (#142)

--- apps/docs-app/config/environment.js
+++ apps/docs-app/config/environment.js
@@ -46,7 +46,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/nrg-ui';
+    ENV.rootURL = '/nrg-ui/';
     ENV.locationType = 'hash';
   }
 
