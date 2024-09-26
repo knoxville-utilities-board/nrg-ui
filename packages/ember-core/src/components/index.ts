40fa67a8a6b085caaceb7df13bd64d23e5bb53c5
TSenter <tyler.senter.427@gmail.com>
2024-09-26 15:50:04 -0400

    feat: Add index for subdirectories

---
+++ packages/ember-core/src/components/index.ts
@@ -0,0 +1,37 @@
+/* Forms */
+export { default as BoundValue } from './form/bound-value.ts';
+export { default as CheckboxGroup } from './form/checkbox-group.gts';
+export { default as Checkbox } from './form/checkbox.gts';
+export { default as Field } from './form/field.gts';
+export { default as Form } from './form/index.gts';
+export { default as PhoneInput } from './form/phone-input.gts';
+export { default as RadioGroup } from './form/radio-group.gts';
+export { default as Select } from './form/select.gts';
+export { default as TextArea } from './form/text-area.gts';
+export { default as TextInput } from './form/text-input.gts';
+
+/* Marketing */
+export { default as MktgCardContainer } from './mktg/card-container.gts';
+export { default as MktgCard } from './mktg/card.gts';
+export { default as MktgFaq } from './mktg/faq.gts';
+export { default as MktgFeatureList } from './mktg/feature-list.gts';
+export { default as MktgFooter } from './mktg/footer.gts';
+export { default as MktgHeader } from './mktg/header.gts';
+export { default as MktgNavbar } from './mktg/navbar.gts';
+export { default as MktgPromoContainer } from './mktg/promo-container.gts';
+export { default as MktgPromo } from './mktg/promo.gts';
+export { default as MktgSectionHeader } from './mktg/section-header.gts';
+export { default as MktgServicePricing } from './mktg/service-pricing.gts';
+export { default as MktgWorkflowTray } from './mktg/workflow-tray.gts';
+
+/* Core */
+export { default as Alert } from './alert.gts';
+export { default as ButtonGroup } from './button-group.gts';
+export { default as Button } from './button.gts';
+export { default as Card } from './card.gts';
+export { default as Footer } from './footer.gts';
+export { default as Header } from './header.gts';
+export { default as Icon } from './icon.gts';
+export { default as LoadingIndicator } from './loading-indicator.gts';
+export { default as NavItem } from './nav-item.gts';
+export { default as Progress } from './progress.gts';
