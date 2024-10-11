c6baf17bad54243ab702ff17da4c42209f7e70ff
Tyler Senter <tyler.senter.427@gmail.com>
2024-10-11 10:08:45 -0400

    feat: Update installation blueprint (#209)
    * chore: Update blueprint name to match package name
    
    * fix(blueprint): Resolve error when installing via `ember install`
    
    * feat(blueprint): Remove `ember-welcome-page`
    
    * feat(blueprint): Add chunks for Embroider apps

--- packages/ember-core/blueprints/@nrg-ui-core/index.js
+++ packages/ember-core/blueprints/@nrg-ui-core/index.js
@@ -21,14 +21,29 @@ module.exports = {
     'The default blueprint for @nrg-ui/core. This runs when you run `ember install @nrg-ui/core` in your project.',
 
   async afterInstall() {
+    await this.removePackagesFromProject([{ name: 'ember-welcome-page' }]);
     await this.addPackagesToProject([{ name: '@nrg-ui/css' }]);
 
+    // Dummy/test apps in addons that might use Embroider
     await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
       before:
         / {2}const { maybeEmbroider } = require\('@embroider\/test-setup'\);/g,
     });
+
+    // Apps using the classic build system
     await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
       before: / {2}return app.toTree\(\);/g,
     });
+
+    // Apps using Embroider as the build system
+    await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
+      before: / {2}const \{ Webpack \} = require\('@embroider\/webpack'\);/g,
+    });
   },
+
+  /* The blueprint base verifies that an entity name is passed to the
+   * blueprint. This is not necessary for this blueprint, so we want
+   * to bypass it.
+   */
+  normalizeEntityName() {},
 };
