117891dea012d745485a477115fe1be7ee58f19e
Keith <keith@clinard.dev>
2024-10-30 11:44:20 -0400

    chore: PR Suggestion
    Co-authored-by: Tyler Senter <tyler.senter.427@gmail.com>

--- packages/theme-generator/src/cleanup.js
+++ packages/theme-generator/src/cleanup.js
@@ -2,6 +2,5 @@ export default function (inputCSS, themeName) {
   const strippedCSS = inputCSS.replace(',[data-bs-theme=light]', '');
   const compressedRootBlocks = strippedCSS.replaceAll('}:root{', ';');
   const themedCSS = compressedRootBlocks.replace(':root', `[data-theme='${themeName}']`);
-  const spacedCSS = themedCSS.replaceAll('}', '}\n\n');
-  return spacedCSS;
+  return themedCSS.replaceAll('}', '}\n\n');
 }
