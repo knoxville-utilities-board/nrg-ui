1859319ae7514b7b8d5619026ba0dff0d712fc27
TSenter <tyler.senter.427@gmail.com>
2024-11-02 01:00:35 -0400

    chore(docs): Remove custom themeing in favor of NRG themeing

--- apps/docs-app/app/controllers/application.ts
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-import { service } from '@ember/service';
-
-import type ApplicationService from '../services/application';
-
-export default class ApplicationController extends Controller {
-  @service
-  declare application: ApplicationService;
-}
