84a204879784f7e83e69306ab1f840a3a7a1968b
TSenter <tyler.senter.427@gmail.com>
2024-11-28 21:05:12 -0500

    chore(test-app): Enable full Glint support

--- apps/test-app/types/global.d.ts
+++ apps/test-app/types/global.d.ts
@@ -1,1 +1,12 @@
 import '@glint/environment-ember-loose';
+import '@glint/environment-ember-template-imports';
+
+import NrgTemplateRegistry from '@nrg-ui/core/template-registry';
+
+declare module '@glint/environment-ember-loose/registry' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export default interface Registry extends NrgTemplateRegistry {
+    // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
+    // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
+  }
+}
