7b762549ebe118150e9351cae00ed9788052ef7c
TSenter <tyler.senter.427@gmail.com>
2024-12-02 15:10:11 -0500

    feat: Create "classes" modifier

--- packages/ember-core/src/modifiers/index.ts
+++ packages/ember-core/src/modifiers/index.ts
@@ -1,3 +1,4 @@
+export { default as classes } from './classes.ts';
 export { default as onClickOutside } from './on-click-outside.ts';
 export { default as onDestroy } from './on-destroy.ts';
 export { default as onInsert } from './on-insert.ts';
