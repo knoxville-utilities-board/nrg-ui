4ea1ff4b6f1d3420b0401be586064b61f3051e27
TSenter <tyler.senter.427@gmail.com>
2024-12-11 09:07:32 -0500

    chore: PR review

--- packages/ember-core/src/modifiers/index.ts
+++ packages/ember-core/src/modifiers/index.ts
@@ -1,4 +1,4 @@
 export { default as onClickOutside } from './on-click-outside.ts';
-export { default as OnDestroy } from './on-destroy.ts';
+export { default as onDestroy } from './on-destroy.ts';
 export { default as onInsert } from './on-insert.ts';
 export { default as onUpdate } from './on-update.ts';
