83697a6e2f47311ea0edbc66652357c4dc839a7e
Tyler Senter <tyler.senter.427@gmail.com>
2024-12-19 22:12:11 -0500

    chore(docs-app): Use stable @embroider/broccoli-side-watch (#310)

--- apps/docs-app/ember-cli-build.js
+++ apps/docs-app/ember-cli-build.js
@@ -1,47 +1,19 @@
 'use strict';
 /* eslint-disable @typescript-eslint/no-require-imports */
 
+const sideWatch = require('@embroider/broccoli-side-watch');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const fs = require('fs');
-const path = require('path');
 
 module.exports = async function (defaults) {
-  const { readPackageUpSync } = await import('read-package-up');
-
   const app = new EmberApp(defaults, {
     minifyCSS: {
       enabled: false,
     },
     'ember-cli-babel': { enableTypeScriptTransform: true },
     trees: {
-      app: (() => {
-        const sideWatch = require('@embroider/broccoli-side-watch');
-
-        const paths = ['@nrg-ui/core', '@nrg-ui/css'].map((libraryName) => {
-          const entry = require.resolve(libraryName);
-          const { packageJson, path: packageJsonPath } = readPackageUpSync({
-            cwd: entry,
-          });
-          const packagePath = path.dirname(packageJsonPath);
-
-          console.debug(
-            `Side-watching ${libraryName} from ${packagePath}, which started in ${entry}`,
-          );
-
-          const toWatch = packageJson.files
-            .map((f) => path.join(packagePath, f))
-            .filter((p) => {
-              if (!fs.existsSync(p)) return false;
-              if (!fs.lstatSync(p).isDirectory()) return false;
-
-              return !p.endsWith('/src');
-            });
-
-          return toWatch;
-        });
-
-        return sideWatch('app', { watching: paths.flat() });
-      })(),
+      app: sideWatch('app', {
+        watching: ['@nrg-ui/core', '@nrg-ui/css'],
+      }),
     },
     '@embroider/macros': {
       setConfig: {
