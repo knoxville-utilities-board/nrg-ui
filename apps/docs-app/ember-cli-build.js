4555fbc9c24699cedc8779899f81c306407e4647
TSenter <tyler.senter.427@gmail.com>
2025-01-07 11:06:44 -0500

    WIP chore: Auto-save

--- apps/docs-app/ember-cli-build.js
+++ apps/docs-app/ember-cli-build.js
@@ -6,6 +6,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = async function (defaults) {
   const app = new EmberApp(defaults, {
+    'ember-freestyle': {
+      includeStyles: false,
+    },
     minifyCSS: {
       enabled: false,
     },
