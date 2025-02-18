2243cb101ad236de0b75eca350ef6583e0ff24f6
TSenter <tyler.senter.427@gmail.com>
2025-02-18 15:04:22 -0500

    feat(docs): Use Webpack to handle style bundling

--- docs/app/app.ts
+++ docs/app/app.ts
@@ -3,6 +3,7 @@ import config from 'docs/config/environment';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 
+import '@nrg-ui/css/main.css';
 import 'docs/assets/css/app.css';
 
 export default class App extends Application {
