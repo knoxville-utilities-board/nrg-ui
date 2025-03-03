bcda67ffe101cb9ecf9ba0c1614d958a35656690
TSenter <tyler.senter.427@gmail.com>
2025-03-03 16:50:17 -0500

    chore: Create empty application for blueprint tests (JS)

---
+++ blueprint-tests/js/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'blueprint-test-js/app';
+import config from 'blueprint-test-js/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
