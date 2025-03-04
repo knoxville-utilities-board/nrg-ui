315b18b3a37900b57484b625b72d7d2d560da4d1
TSenter <tyler.senter.427@gmail.com>
2025-03-04 11:01:09 -0500

    WIP chore: Auto-save

--- packages/ember-core/blueprints/@nrg-ui-core/files/__root__/ember-cli-build.js
+++ packages/ember-core/blueprints/@nrg-ui-core/files/__root__/ember-cli-build.js
@@ -15,20 +15,41 @@ module.exports = function (defaults) {
         },
       },
     },
+    babel: {
+      plugins: [
+        // ... any other plugins
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+
+        // NOTE: put any code coverage plugins last, after the transform.
+      ],
+    },
   });
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
     staticAddonTestSupportTrees: true,
     staticAddonTrees: true,
-    staticHelpers: true,
-    staticModifiers: true,
-    staticComponents: true,
+    staticInvokables: true,
     staticEmberSource: true,
     skipBabel: [
       {
         package: 'qunit',
       },
     ],
+    packagerOptions: {
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /\.(woff|woff2|eot|ttf|otf)$/i,
+              type: 'asset/resource',
+              generator: {
+                filename: 'assets/fonts/[name].[hash][ext]',
+              },
+            },
+          ],
+        },
+      },
+    },
   });
 };
--- packages/ember-core/blueprints/@nrg-ui-core/index.js
+++ packages/ember-core/blueprints/@nrg-ui-core/index.js
@@ -1,93 +1,18 @@
 /* eslint-env node */
 'use strict';
-/* eslint-disable @typescript-eslint/no-require-imports */
-
-const { existsSync, readFileSync } = require('fs');
-const { join } = require('path');
-
-const EMBROIDER_CONFIG_CHUNK = `'@embroider/macros': {
-      setConfig: {`;
 
 module.exports = {
   description:
     'The default blueprint for @nrg-ui/core. This runs when you run `ember install @nrg-ui/core` in your project.',
-
-  fileContents: {},
-
-  getFileContents(fileName, options = {}) {
-    Object.assign(options, {
-      root: this.project.root,
-      force: false,
-    });
-
-    const filePath = join(options.root, fileName);
-
-    if (this.fileContents[filePath] && !options.force) {
-      return this.fileContents[filePath];
-    }
-
-    if (!existsSync(filePath)) {
-      return null;
-    }
-
-    const contents = readFileSync(filePath, { encoding: 'utf8' });
-    return (this.fileContents[filePath] = contents);
-  },
-
-  async setupAppVersion() {
-    const buildFileName = 'ember-cli-build.js';
-    const buildFile = this.getFileContents(buildFileName);
-
-    await this.insertIntoFile(
-      'ember-cli-build.js',
-      "\nconst { getVersion } = require('@nrg-ui/version');",
-      {
-        before: "const EmberApp = require('ember-cli/lib/broccoli/ember-app');",
-      },
-    );
-
-    if (!buildFile.matches(new RegExp(EMBROIDER_CONFIG_CHUNK))) {
-      await this.insertIntoFile(
-        buildFileName,
-        `'@embroider/macros': {
-      setConfig: {`,
-        {
-          before: `});`,
-        },
-      );
-    }
-    await this.insertIntoFile(
-      buildFileName,
-      `'@nrg-ui/core': {
-        appVersion: getVersion(),
-      },`,
-      {
-        after: EMBROIDER_CONFIG_CHUNK,
-      },
-    );
-  },
+  shouldTransformTypeScript: true,
 
   async afterInstall() {
     await this.removePackagesFromProject([{ name: 'ember-welcome-page' }]);
-    await this.addPackagesToProject([{ name: '@nrg-ui/css' }]);
-
-    await this.setupAppVersion();
-
-    // Dummy/test apps in addons that might use Embroider
-    await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
-      before:
-        / {2}const { maybeEmbroider } = require\('@embroider\/test-setup'\);/g,
-    });
-
-    // Apps using the classic build system
-    await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
-      before: / {2}return app.toTree\(\);/g,
-    });
-
-    // Apps using Embroider as the build system
-    await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
-      before: / {2}const \{ Webpack \} = require\('@embroider\/webpack'\);/g,
-    });
+    await this.addPackagesToProject([
+      { name: '@nrg-ui/css' },
+      { name: '@nrg-ui/version' },
+      { name: 'ember-concurrency', target: '^4.0.3' },
+    ]);
   },
 
   /* The blueprint base verifies that an entity name is passed to the
@@ -95,4 +20,15 @@ module.exports = {
    * to bypass it.
    */
   normalizeEntityName() {},
+
+  fileMapTokens() {
+    return {
+      __root__() {
+        return '/';
+      },
+      __app__() {
+        return '/app/';
+      },
+    };
+  },
 };
