99a893c6e7d28f38e36a2e2917efc9b1a9149b73
TSenter <tyler.senter.427@gmail.com>
2025-03-04 16:16:57 -0500

    feat: Update blueprint to use Webpack for CSS

---
+++ packages/ember-core/blueprints/@nrg-ui-core/files/__root__/ember-cli-build.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const { getVersion } = require('@nrg-ui/version');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function (defaults) {
+  const app = new EmberApp(defaults, {
+    'ember-cli-babel': { enableTypeScriptTransform: true },
+
+    // Add options here
+    '@embroider/macros': {
+      setConfig: {
+        '@nrg-ui/core': {
+          appVersion: getVersion(),
+        },
+      },
+    },
+    babel: {
+      plugins: [
+        // ... any other plugins
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+
+        // NOTE: put any code coverage plugins last, after the transform.
+      ],
+    },
+  });
+
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticInvokables: true,
+    staticEmberSource: true,
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+    packagerOptions: {
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /\.(woff|woff2|eot|ttf|otf)$/i,
+              type: 'asset/resource',
+              generator: {
+                filename: 'assets/fonts/[name].[hash][ext]',
+              },
+            },
+          ],
+        },
+      },
+    },
+  });
+};
--- packages/ember-core/blueprints/@nrg-ui-core/index.js
+++ packages/ember-core/blueprints/@nrg-ui-core/index.js
@@ -1,44 +1,50 @@
-/* eslint-env node */
 'use strict';
 
-const BUILD_CHUNK = `  app.import('node_modules/@nrg-ui/css/dist/main.css');
-  app.import(
-    'node_modules/@nrg-ui/css/dist/assets/icons/fonts/bootstrap-icons.woff',
-    {
-      destDir: 'assets/fonts',
-    },
-  );
-  app.import(
-    'node_modules/@nrg-ui/css/dist/assets/icons/fonts/bootstrap-icons.woff2',
-    {
-      destDir: 'assets/fonts',
-    },
-  );
-`;
+/* eslint-disable @typescript-eslint/no-require-imports */
+const chalk = require('chalk');
+const { existsSync, mkdirSync, renameSync, rmdirSync } = require('node:fs');
+const { join } = require('node:path');
+const { cwd } = require('node:process');
+
+const APP = 'app';
+const prefix = chalk.blue('[@nrg-ui/core] ');
 
 module.exports = {
   description:
     'The default blueprint for @nrg-ui/core. This runs when you run `ember install @nrg-ui/core` in your project.',
+  shouldTransformTypeScript: true,
 
   async afterInstall() {
     await this.removePackagesFromProject([{ name: 'ember-welcome-page' }]);
-    await this.addPackagesToProject([{ name: '@nrg-ui/css' }]);
+    await this.addPackagesToProject([
+      { name: '@nrg-ui/css' },
+      { name: '@nrg-ui/version' },
+      { name: 'dayjs', target: '^1.11.13' },
+      { name: 'ember-concurrency', target: '^4.0.0' },
+      { name: 'ember-css-transitions', target: '^4.4.0' },
+      { name: 'ember-intl', target: '^7.0.0' },
+      { name: 'ember-keyboard', target: '^8.2.0' },
+      { name: 'ember-lifeline', target: '^7.0.0' },
+      { name: 'ismobilejs', target: '^1.1.1' },
+    ]);
 
-    // Dummy/test apps in addons that might use Embroider
-    await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
-      before:
-        / {2}const { maybeEmbroider } = require\('@embroider\/test-setup'\);/g,
-    });
+    this.ui.writeLine(prefix + chalk.green('Configuring assets...'));
 
-    // Apps using the classic build system
-    await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
-      before: / {2}return app.toTree\(\);/g,
-    });
+    const stylesDir = join(APP, 'styles');
+    const assetsDir = join(APP, 'assets');
+    const cssDir = join(assetsDir, 'css');
+    const defaultCss = join(stylesDir, 'app.css');
 
-    // Apps using Embroider as the build system
-    await this.insertIntoFile('ember-cli-build.js', BUILD_CHUNK, {
-      before: / {2}const \{ Webpack \} = require\('@embroider\/webpack'\);/g,
-    });
+    mkdirSync(cssDir, { recursive: true });
+    if (existsSync(defaultCss)) {
+      renameSync(defaultCss, join(cssDir, 'app.css'));
+    }
+
+    try {
+      rmdirSync(stylesDir);
+    } catch {
+      // The directory isn't empty or we don't have permission
+    }
   },
 
   /* The blueprint base verifies that an entity name is passed to the
@@ -46,4 +52,24 @@ module.exports = {
    * to bypass it.
    */
   normalizeEntityName() {},
+
+  locals() {
+    const environment = require(join(cwd(), 'config', 'environment.js'));
+    const { modulePrefix } = environment();
+
+    return {
+      modulePrefix,
+    };
+  },
+
+  fileMapTokens() {
+    return {
+      __root__() {
+        return '/';
+      },
+      __app__() {
+        return '/app/';
+      },
+    };
+  },
 };
