59d8073187ad1d85640f116be6cc2460e2d10cc4
TSenter <tyler.senter.427@gmail.com>
2025-04-17 09:58:21 -0400

    WIP chore: Auto-save

--- packages/ember-core/addon-main.cjs
+++ packages/ember-core/addon-main.cjs
@@ -2,4 +2,6 @@
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { addonV1Shim } = require('@embroider/addon-shim');
-module.exports = addonV1Shim(__dirname);
+module.exports = {
+  ...addonV1Shim(__dirname),
+};
