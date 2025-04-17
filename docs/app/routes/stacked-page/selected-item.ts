5e406a4de10144ddf712a61318ca21404ef0df10
jkc07100 <keith.clinard@kub.org>
2025-04-17 14:12:49 -0400

    chore: Add stacked page sample

---
+++ docs/app/routes/stacked-page/selected-item.ts
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default class SelectedItemRoute extends Route {
+  async model(params: Record<string, unknown>) {
+    return params['item_id'];
+  }
+}
