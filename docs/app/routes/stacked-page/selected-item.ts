f712894846fbcb925191f96447248c3c6122e99d
KeithClinard <keith@clinard.dev>
2025-05-01 13:40:59 -0400

    fix: Typescript error

--- docs/app/routes/stacked-page/selected-item.ts
+++ docs/app/routes/stacked-page/selected-item.ts
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class SelectedItemRoute extends Route {
   async model(params: Record<string, unknown>) {
-    return params['item_id'];
+    return params['item_id'] as string;
   }
 }
--- docs/app/routes/stacked-page/selected-item/selected-subitem.ts
+++ docs/app/routes/stacked-page/selected-item/selected-subitem.ts
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class SelectedSubitemRoute extends Route {
   async model(params: Record<string, unknown>) {
-    return params['subitem_id'];
+    return params['subitem_id'] as string;
   }
 }
