7cdaeb45519e56084400eb05e61fa608692a788d
Tyler Senter <tyler.senter.427@gmail.com>
2025-05-09 13:00:34 -0400

    chore(ci): Enable testing for Ember 6.4 (#465)
    With 6.4 being released and only a couple months from LTS, we should start testing this new version in preparation. Once 6.4 is moved to LTS, we should mark this new test as required.

--- docs/config/ember-try.js
+++ docs/config/ember-try.js
@@ -29,6 +29,14 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-6.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~6.4.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
