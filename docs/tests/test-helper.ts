e053a975ba2ea267c910cf00983d3302ebfd57ab
TSenter <tyler.senter.427@gmail.com>
2025-07-03 11:57:08 -0400

    chore: Upgrade ember-qunit

--- docs/tests/test-helper.ts
+++ docs/tests/test-helper.ts
@@ -1,7 +1,8 @@
 import { setApplication } from '@ember/test-helpers';
 import Application from 'docs/app';
 import config from 'docs/config/environment';
-import { start } from 'ember-qunit';
+import { setupEmberOnerrorValidation, start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 
@@ -9,4 +10,6 @@ setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
 
+setupEmberOnerrorValidation();
+loadTests();
 start();
