6d114e795f7bc51c8509449c96af9d2d062a6bf3
TSenter <tyler.senter.427@gmail.com>
2025-07-03 14:56:09 -0400

    chore: Run `pnpx ember-vite-codemod --ts`

--- docs/app/app.ts
+++ docs/app/app.ts
@@ -1,15 +1,17 @@
 import Application from '@ember/application';
-import config from 'docs/config/environment';
+import config from "./config/environment";
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 
 import '@nrg-ui/css/main.css';
 import 'docs/assets/css/app.css';
 
+import compatModules from "@embroider/virtual/compat-modules";
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+  Resolver = Resolver.withModules(compatModules);
 }
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(App, config.modulePrefix, compatModules);
