d42db4f17803f314e1428c67801dde163cb58faa
TSenter <tyler.senter.427@gmail.com>
2025-07-03 15:03:40 -0400

    chore: Resolve linting issues

--- docs/app/app.ts
+++ docs/app/app.ts
@@ -1,12 +1,12 @@
 import Application from '@ember/application';
-import config from "./config/environment";
+import compatModules from '@embroider/virtual/compat-modules';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 
+import config from './config/environment';
+
 import '@nrg-ui/css/main.css';
 import 'docs/assets/css/app.css';
-
-import compatModules from "@embroider/virtual/compat-modules";
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
