abd3810d543d8b7a6837e2cc23f75d762e88420f
TSenter <tyler.senter.427@gmail.com>
2025-07-03 15:03:40 -0400

    chore: Resolve linting issues

--- docs/app/controllers/application.ts
+++ docs/app/controllers/application.ts
@@ -1,8 +1,6 @@
 import Controller from '@ember/controller';
 import { isDevelopingApp, isTesting, macroCondition } from '@embroider/macros';
 
-// @ts-expect-error - Currently don't know how to
-// handle SVG imports in TypeScript
 import logo from '../assets/images/logo.svg';
 
 export default class ApplicationController extends Controller {
