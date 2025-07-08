34d12369f2af890b2757d816a7e3d4f4f8e6ac4d
TSenter <tyler.senter.427@gmail.com>
2025-07-08 16:16:56 -0400

    chore: Upgrade docs to Ember.js 6.4

--- docs/app/app.ts
+++ docs/app/app.ts
@@ -1,10 +1,15 @@
 import Application from '@ember/application';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import config from 'docs/config/environment';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 
 import '@nrg-ui/css/main.css';
 import 'docs/assets/css/app.css';
+
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow.ts');
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
