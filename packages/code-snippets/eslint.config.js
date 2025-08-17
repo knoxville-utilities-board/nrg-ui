758d2422027bc699e9096c2cf512680669a7034d
TSenter <tyler.senter.427@gmail.com>
2025-08-17 02:25:39 -0400

    chore: Add test coverage

--- packages/code-snippets/eslint.config.js
+++ packages/code-snippets/eslint.config.js
@@ -1,7 +1,7 @@
 import { eslint, merge } from '@nrg-ui/standards';
 
 export default await merge(
-  eslint.rules.ignore([]),
+  eslint.rules.ignore(['coverage', 'tests/fixtures']),
   eslint.rules.base(),
   eslint.rules.js(),
   eslint.rules.ts(),
