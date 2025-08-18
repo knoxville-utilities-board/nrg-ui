33817150eacd9cee2b68390fc674ab747c548da4
TSenter <tyler.senter.427@gmail.com>
2025-08-18 12:02:28 -0400

    chore(ci): Add code coverage report

--- packages/code-snippets/vitest.config.ts
+++ packages/code-snippets/vitest.config.ts
@@ -2,6 +2,21 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      reporter: ['text', 'html', 'clover', 'json', 'json-summary'],
+      thresholds: {
+        branches: 70,
+        functions: 70,
+        lines: 70,
+        statements: 70,
+      },
+      watermarks: {
+        branches: [70, 85],
+        functions: [70, 85],
+        lines: [70, 85],
+        statements: [70, 85],
+      },
+    },
     globals: true,
     environment: 'node',
     include: ['tests/**/*.ts'],
