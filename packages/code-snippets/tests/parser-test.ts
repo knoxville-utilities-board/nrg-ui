5469bc0ef5e23acc839f889bbe5878535d51eb8f
Tyler Senter <tyler.senter.427@gmail.com>
2025-08-21 10:15:29 -0400

    feat: Trim leading whitespace to match first line (#575)
    * feat: Trim leading whitespace to match first line
    
    * chore: Add tests

--- packages/code-snippets/tests/parser-test.ts
+++ packages/code-snippets/tests/parser-test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import { extractSnippetsFromCode } from '../src/parser.js';
 
@@ -73,5 +73,59 @@ console.log("Foo");
       endRegex,
     );
     expect(result).toHaveLength(0);
+  });
+
+  it('strips leading whitespace', () => {
+    const code = `
+    // BEGIN-SNIPPET hello
+    for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i]);
+    }
+    // END-SNIPPET`;
+
+    const result = extractSnippetsFromCode(
+      'file.js',
+      code,
+      startRegex,
+      endRegex,
+    );
+    expect(result).toHaveLength(1);
+
+    const [snippet] = result;
+    expect(snippet.name).toBe('hello');
+    expect(snippet.code).toBe(
+      `for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}`,
+    );
+    expect(snippet.sources).toHaveLength(1);
+
+    expect(snippet.sources[0].location.file).toBe('file.js');
+    expect(snippet.sources[0].location.lines.start).toBe(3);
+    expect(snippet.sources[0].location.lines.end).toBe(5);
+  });
+
+  it('skips empty snippets', () => {
+    const consoleMock = vi
+      .spyOn(console, 'warn')
+      .mockImplementation(() => null);
+
+    const code = `
+    // BEGIN-SNIPPET hello
+    // END-SNIPPET`;
+
+    const result = extractSnippetsFromCode(
+      'file.js',
+      code,
+      startRegex,
+      endRegex,
+    );
+    expect(result).toHaveLength(0);
+
+    expect(consoleMock).toHaveBeenCalledWith(
+      `Snippet "hello" in file "file.js" is empty. It will not be included in the output.`,
+    );
+
+    consoleMock.mockRestore();
   });
 });
