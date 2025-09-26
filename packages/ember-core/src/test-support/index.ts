403f3ea7922524f79213b40c26e9536c897915b9
TSenter <tyler.senter.427@gmail.com>
2025-09-26 14:51:35 -0400

    fix: Update test helper

--- packages/ember-core/src/test-support/index.ts
+++ packages/ember-core/src/test-support/index.ts
@@ -1,29 +1,22 @@
 import { getContext, settled } from '@ember/test-helpers';
-import { TrackedSet } from 'tracked-built-ins';
 
 import type Media from '../services/media.ts';
 import type Owner from '@ember/owner';
 
 export async function setBreakpoint(breakpoint: string) {
-  const breakpointArray = Array.isArray(breakpoint) ? breakpoint : [breakpoint];
   const { owner } = getContext() as { owner: Owner };
   const media = owner.lookup('service:media') as Media;
 
-  for (let i = 0; i < breakpointArray.length; i++) {
-    const breakpointName = breakpointArray[i];
-    if (breakpointName === 'auto') {
-      media.mocked = false;
-      return;
-    }
-
-    if (Object.keys(media.breakpoints).indexOf(breakpointName) === -1) {
-      throw new Error(
-        `Breakpoint "${breakpointName}" not defined as a breakpoint`,
-      );
-    }
+  if (breakpoint === 'auto') {
+    media.mocked = false;
+    return;
   }
 
-  media.matches = new TrackedSet(breakpointArray);
+  if (Object.keys(media.breakpoints).indexOf(breakpoint) === -1) {
+    throw new Error(`Breakpoint "${breakpoint}" not defined as a breakpoint`);
+  }
+
+  media._mockedBreakpoint = breakpoint;
   media.trigger('mediaChanged');
 
   await settled();
