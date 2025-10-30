8dcf895a1b34b6c92c8abaf7aba3ec6f235ea827
Tyler Senter <tyler.senter.427@gmail.com>
2025-10-30 08:48:54 -0400

    feat: Add code-snippet helper (#671)
    * refactor: Update virtual module name
    
    * chore: Add `@nrg-ui/code-snippets` as peer dependency
    
    * feat: Publish types for virtual module
    
    * feat: Add code-snippet helper

--- packages/ember-core/src/helpers/index.ts
+++ packages/ember-core/src/helpers/index.ts
@@ -1,5 +1,6 @@
 import Bind, { bind } from './bind.ts';
 import Classes, { classes } from './classes.ts';
+import { codeSnippet } from './code-snippet.ts';
 import lookupService from './lookup-service.ts';
 import Version, { version } from './version.ts';
 
@@ -9,6 +10,7 @@ export default {
   Version,
   bind,
   classes,
+  codeSnippet,
   lookupService,
   version,
 };
