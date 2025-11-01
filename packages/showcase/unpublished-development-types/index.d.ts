3b464c0f4d90b83eaa34590f2b4bd77202e1fcdb
TSenter <tyler.senter.427@gmail.com>
2025-11-01 16:39:55 -0400

    feat: Add component for code blocks with Shiki.js

--- packages/showcase/unpublished-development-types/index.d.ts
+++ packages/showcase/unpublished-development-types/index.d.ts
@@ -1,5 +1,18 @@
 // Add any types here that you need for local development only.
 // These will *not* be published as part of your addon, so be careful that your published code does not rely on them!
+import type { BundledLanguage, BundledTheme } from 'shiki';
 
 import '@glint/ember-tsc/types';
 import 'ember-source/types';
+
+export interface EmbroiderOptions {
+  languages?: BundledLanguage[];
+  themes?: {
+    light?: BundledTheme[];
+    dark?: BundledTheme[];
+  };
+}
+
+declare module '@embroider/macros' {
+  function getOwnConfig(): EmbroiderOptions;
+}
