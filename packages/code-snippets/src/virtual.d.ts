047ee070ad9d262e578818e2fba4364c0bff904f
TSenter <tyler.senter.427@gmail.com>
2025-11-10 16:16:31 -0500

    feat(types): Convert virtual module to real module

--- packages/code-snippets/src/virtual.d.ts
+++
@@ -1,4 +0,0 @@
-import type { SnippetEntry } from './types.d.ts';
-
-declare const snippets: Record<string, SnippetEntry>;
-export default snippets;
---
+++ packages/code-snippets/src/virtual.ts
@@ -0,0 +1,4 @@
+import type { SnippetEntry } from './types.ts';
+
+export const snippets: Record<string, SnippetEntry> = {};
+export default snippets;
