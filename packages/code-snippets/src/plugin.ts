82b5bc9c7d1b48cf9e6815923b7b21549250564c
TSenter <tyler.senter.427@gmail.com>
2025-11-13 12:29:13 -0500

    fix(plugin): Write virtual content as object instead of array

--- packages/code-snippets/src/plugin.ts
+++ packages/code-snippets/src/plugin.ts
@@ -56,7 +56,7 @@ export default function codeSnippetsPlugin(
 
     load(id: string) {
       if (id === resolvedVirtualModuleId) {
-        return `export default ${JSON.stringify(Array.from(snippets.values()), null, 2)};`;
+        return `export default ${JSON.stringify(Object.fromEntries(snippets.entries()), null, 2)};`;
       }
     },
 
