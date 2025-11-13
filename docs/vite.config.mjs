fab5aa44590f06df88e17262c53d9dc702159185
TSenter <tyler.senter.427@gmail.com>
2025-11-12 23:33:09 -0500

    feat: Add code blocks Vite plugin

--- docs/vite.config.mjs
+++ docs/vite.config.mjs
@@ -1,9 +1,11 @@
 import { classicEmberSupport, ember, extensions } from '@embroider/vite';
+import { extractCodeBlocks } from '@nrg-ui/showcase/plugins';
 import { babel } from '@rollup/plugin-babel';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
+    extractCodeBlocks(),
     classicEmberSupport(),
     ember(),
     // extra plugins here
