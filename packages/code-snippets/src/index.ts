2211435bf0c7dc8abbd98cde64aa7c79acd38fce
TSenter <tyler.senter.427@gmail.com>
2025-11-20 15:19:16 -0500

    chore: Remove helper export
    If the helper export from the root index is used in the consuming app's
    code, Vite treats the entire module as code to be executed in the
    browser. This causes build errors. Removing the export from the root
    index forces users to use the full path to the helper, which _is_ valid
    browser code.

--- packages/code-snippets/src/index.ts
+++ packages/code-snippets/src/index.ts
@@ -1,6 +1,5 @@
 export const virtualModule = '@nrg-ui/code-snippets/virtual';
 
-export { getSnippet } from './helper.js';
 export { default as codeSnippetsPlugin } from './plugin.js';
 
 export type {
