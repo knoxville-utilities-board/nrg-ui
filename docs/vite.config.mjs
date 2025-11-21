37396edb0442387347bb34854af1c1911a2d934b
Tyler Senter <tyler.senter.427@gmail.com>
2025-11-21 13:17:50 -0500

    fix: Set base URL for docs app (#743)

--- docs/vite.config.mjs
+++ docs/vite.config.mjs
@@ -2,18 +2,37 @@ import { classicEmberSupport, ember, extensions } from '@embroider/vite';
 import { codeSnippetsPlugin } from '@nrg-ui/code-snippets';
 import { extractCodeBlocks } from '@nrg-ui/showcase/plugins';
 import { babel } from '@rollup/plugin-babel';
+import { join, resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [
-    extractCodeBlocks(),
-    codeSnippetsPlugin(),
-    classicEmberSupport(),
-    ember(),
-    // extra plugins here
-    babel({
-      babelHelpers: 'runtime',
-      extensions,
-    }),
-  ],
+export default defineConfig(async (config) => {
+  const { mode } = config;
+  let emberEnvironment = 'production';
+
+  if (mode === 'development') {
+    emberEnvironment = 'development';
+  } else if (mode === 'test') {
+    emberEnvironment = 'test';
+  }
+
+  const { default: emberEnv } = await import(
+    resolve(join('config', 'environment.js'))
+  );
+  const resolvedEmberEnv = await emberEnv(emberEnvironment);
+
+  config.base = resolvedEmberEnv.rootURL ?? '/';
+
+  return {
+    plugins: [
+      extractCodeBlocks(),
+      codeSnippetsPlugin(),
+      classicEmberSupport(),
+      ember(),
+      // extra plugins here
+      babel({
+        babelHelpers: 'runtime',
+        extensions,
+      }),
+    ],
+  };
 });
