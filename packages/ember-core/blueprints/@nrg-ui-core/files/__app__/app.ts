1bab9f17d4be11bfa53e0ea488f50993b4b70d7e
TSenter <tyler.senter.427@gmail.com>
2025-11-25 09:16:40 -0500

    feat: Update install blueprint to use Vite

--- packages/ember-core/blueprints/@nrg-ui-core/files/__app__/app.ts
+++ packages/ember-core/blueprints/@nrg-ui-core/files/__app__/app.ts
@@ -1,7 +1,10 @@
 import Application from '@ember/application';
-import Resolver from 'ember-resolver';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+import compatModules from '@embroider/virtual/compat-modules';
 import loadInitializers from 'ember-load-initializers';
-import config from '<%= modulePrefix %>/config/environment';
+import Resolver from 'ember-resolver';
+
+import config from './config/environment.ts';
 
 import '@nrg-ui/css/main.css';
 import '<%= modulePrefix %>/assets/css/app.css';
@@ -9,7 +12,7 @@ import '<%= modulePrefix %>/assets/css/app.css';
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+  Resolver = Resolver.withModules(compatModules);
 }
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(App, config.modulePrefix, compatModules);
