a94d21a5be1edb2932cb2a74c8b786b56e1b8403
TSenter <tyler.senter.427@gmail.com>
2025-12-11 17:07:12 -0500

    chore: Resolve intl linting issues

---
+++ packages/ember-core/ember-intl.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  lintRules: {
+    // Currently can't follow all paths that determine a key,
+    // like in validations. This is likely a difficult, if
+    // not impossible, problem to solve.
+    'no-unused-keys': false,
+  },
+};
